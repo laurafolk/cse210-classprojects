380561ef17e15f206cf80549afb7920f8231829b
laurafolk <laurafolk@yahoo.com>
2023-04-17 18:44:54 -0500

    Completed core requirements Prep2.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int percentage = int.Parse(answer);
+
+        string grade = "";
+
+        if (percentage >= 90)
+        {
+            grade = "A";
+        }
+        else if (percentage >= 80)
+        {
+            grade = "B";
+        }
+        else if (percentage >= 70)
+        {
+            grade = "C";
+        }
+        else if (percentage >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+        Console.WriteLine($"Your grade is: {grade}");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulations!  You have a passing grade.");
+        }
+        else
+            Console.WriteLine("That is ok try harder and do better next time.");
     }
 }
