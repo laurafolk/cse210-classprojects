7bfe2671c0c20dcbba4143c31f50fa7688816a7f
laurafolk <laurafolk@yahoo.com>
2023-04-22 16:55:23 -0500

    magic number game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("What is the magic number? ");
+        // int magicNumber = int.Parse(Console.ReadLine());
+        
+        Random randomGenerator = new Random(); //for core requirement 3
+        int magicNumber = randomGenerator.Next(1, 101); //for core requirement 3
+
+        int guess = -1;
+ 
+        while (guess != magicNumber) //For the computer picking the number.
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }                    
     }
 }
+
+//keep_playing = 'yes' //this is for core requirement 2
+
+//while keep_playing == 'yes': //(this is for core requirement 2 as long as they want to keep playing the game will run.
+    
+//print(f'It took {guess_count} guesses. ') // Stretch challenge 1.
+        
+//keep_playing = input('Would you like to play again (yes/no)? ') //Stretch challenge 2
+    
+//Console.WriteLine('Thank you for playing the game.  Goodbye. ') //Message for end of the game.
